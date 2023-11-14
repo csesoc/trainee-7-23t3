76a2f1bab2b2a09f7c1e2abf3c25c54c0ee630a1
SkellyBG <skellybg245@gmail.com>
2023-11-14 21:21:19 +1100

    config: more jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  moduleNameMapper: {
+    '^@datatypes/(.*)$': '<rootDir>/src/types/$1',
+  },
+}
+
+export default jestConfig
